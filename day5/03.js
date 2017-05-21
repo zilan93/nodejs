4a667a737840f61dacb5e1df80c26dbc218e38ea
yangqin <zilan93@126.com>
2017-05-21 21:50:08 +0800

    20170521

---
+++ day5/03.js
@@ -0,0 +1,45 @@
+/*留言本*/
+/*
+*用户可以留言；【将数据保存到数据库】
+*用户可以编辑留言；
+*管理员可以删除留言数据；
+*/
+
+var express = require("express")
+var app = express();
+var formidable = require("formidable");
+var db = require("./model/db.js");
+//静态文件
+app.use(express.static("views"));
+//设置模板引擎
+app.set("view engine","jade");
+//显示留言
+app.get("/",function(req,res,next) {
+	res.render("index",{"name":"hehe"});
+});
+app.get("/read",function(req,res,next) {
+	db.find("liuyanben",{},{"pageCount":10},function(err,result) {
+		if(err) {
+			res.json("-1");
+			return;
+		};
+		res.json({"result":result});
+	})
+})
+app.post("/tijiao",function(req,res,next) {
+	var form = new formidable();
+	form.parse(req,function(err,fields) {
+		//写入数据库
+		db.insertOne("liuyanben",{
+			"name":fields.name,
+			"msg":fields.msg
+		},function(err,result) {
+			if(err) {
+				res.json("-1");
+				return;
+			}
+			res.json("1");
+		})
+	});
+});
+app.listen(3000);
--- day5/model/db.js
+++ day5/model/db.js
@@ -2,7 +2,7 @@ var MongoClient = require("mongodb").MongoClient;
 
 //内部函数--连接数据库
 function _connectDB(callback) {
-	var url = 'mongodb://localhost:27017/haha';
+	var url = 'mongodb://localhost:27017/liuyanben';
 	MongoClient.connect(url,function(err,db) {
 		callback(err,db);
 	});
@@ -23,9 +23,11 @@ exports.find = function(collectionName,json,args,callback) {
 		callback("error",null);
 		return;
 	}
-	var pageCount = args.pageCount;
-	var skip = args.pageCount * args.pages;
 	_connectDB(function(err,db) {
+		var count = db.collection(collectionName).find({}).count();
+		var pageCount = args.pageCount;
+		var pages = Math.ceil(count / pageCount);
+		var skip = args.pageCount * (pages-1);
 		var cursor = db.collection(collectionName).find(json).limit(pageCount).skip(skip);
 		cursor.each(function(err,doc) {
 			if(doc != null) {
@@ -46,3 +48,16 @@ exports.removeMany = function(collection,json,callback) {
 		})
 	})
 };
+//修改
+exports.updateMany = function(collectionName,json1,json2,callback) {
+	_connectDB(function(err,db) {
+		db.collection(collectionName).updateMany(
+				json1,
+				json2,
+				function(err,results) {
+					callback(err,results);
+					db.close();
+				}
+			)
+	})
+}
